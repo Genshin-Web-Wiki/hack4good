ab93f3c178d651bce7f601bd95a08725b8206f3a
Danny <dannym.mejia@gmail.com>
2023-05-01 15:14:47 +0200

    Test controller

---
+++ src/main/java/com/hack4good/hackathon/controllers/userController.java
@@ -0,0 +1,12 @@
+package com.hack4good.hackathon.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+    @GetMapping
+    public  String demo() {
+        return "Hello worlds";
+    }
+}
